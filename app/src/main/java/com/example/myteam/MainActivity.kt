0999db497f5b5672d968f4468264efc407cdb2fb
Hsu <a1073340@mail.nuk.edu.tw>
2021-08-24 21:04:37 +0800

    加入第一個頁面跳轉按鈕

--- app/src/main/java/com/example/myteam/MainActivity.kt
+++ app/src/main/java/com/example/myteam/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myteam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initialize
+        val mButton = findViewById<Button>(R.id.button)
+        //handle onClick
+        mButton.setOnClickListener {
+            //intent to start NewActivity
+            startActivity(Intent(this@MainActivity, login::class.java))
+        }
     }
+
 }
