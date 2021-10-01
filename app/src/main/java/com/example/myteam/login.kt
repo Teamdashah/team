e85f5ff9eabf8c68c23f1381d5af4de578ea4d24
Hsu <a1073340@mail.nuk.edu.tw>
2021-10-01 11:57:45 +0800

    取得帳號跟密碼輸入

--- app/src/main/java/com/example/myteam/login.kt
+++ app/src/main/java/com/example/myteam/login.kt
@@ -2,11 +2,24 @@ package com.example.myteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class login : AppCompatActivity() {
+
+    private lateinit var accountEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private  lateinit var loginButton: Button
+    private  lateinit var forgetPasswordButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        accountEditText = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        passwordEditText = findViewById<EditText>(R.id.editTextTextPassword)
+        loginButton = findViewById(R.id.button)
+        forgetPasswordButton = findViewById(R.id.button2)
     }
 }
 
