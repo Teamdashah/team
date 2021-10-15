82aeb791acfe446e49790cacf34622d523aceff0
Ou ZhengHou <bunny8811033@gmail.com>
2021-10-15 17:04:26 +0800

    check_box

---
+++ app/src/main/java/com/example/myteam/restaurant_list.kt
@@ -0,0 +1,11 @@
+package com.example.myteam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class restaurant_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_list)
+    }
+}
