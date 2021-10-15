e754dcad995521e3084afcd5dd357561ecf4d3ee
Ou ZhengHou <bunny8811033@gmail.com>
2021-10-15 15:46:06 +0800

    Date

--- app/src/main/java/com/example/myteam/input_schedule.kt
+++ app/src/main/java/com/example/myteam/input_schedule.kt
@@ -1,11 +1,38 @@
 package com.example.myteam
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class input_schedule : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_schedule)
+
+        val c =Calendar.getInstance()
+        val year =c.get(Calendar.YEAR)
+        val month =c.get(Calendar.MONTH)
+        val day =c.get(Calendar.DAY_OF_MONTH)
+
+        val Pick_Button = findViewById<Button>(R.id.pickDateBtn)
+        //handle onClick
+        Pick_Button.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view: DatePicker?, mYear: Int, mMonth: Int, mDay: Int ->
+                val textView = findViewById<TextView>(R.id.showdate)
+                textView.setText("" + mDay + "/" + mMonth + "/" + mYear)
+            },year,month,day)
+
+            dpd.show()
+        }
+
     }
 }
