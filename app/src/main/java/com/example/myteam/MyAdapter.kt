e807dc6a9fc1b262e5504ae3d0f8aeeb47e495bb
Ou ZhengHou <bunny8811033@gmail.com>
2021-10-23 15:47:06 +0800

    recyclerview

---
+++ app/src/main/java/com/example/myteam/MyAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.myteam
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.row.view.*
+
+class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) :
+    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindItems(model: Model) {
+            itemView.titleTv.text = model.title
+            itemView.descriptionTv.text = model.descriptor
+            itemView.imageIv.setImageResource(model.imageDrawable)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItems(arrayList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+}
--- app/src/main/java/com/example/myteam/restaurant_list.kt
+++ app/src/main/java/com/example/myteam/restaurant_list.kt
@@ -2,10 +2,24 @@ package com.example.myteam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_restaurant_list.*
 
 class restaurant_list : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_list)
+
+        val arrayList = ArrayList<Model>()
+
+        arrayList.add(Model("1", "11", R.drawable.main_foot))
+        arrayList.add(Model("2", "222", R.drawable.main_foot))
+        arrayList.add(Model("3", "33", R.drawable.main_foot))
+        arrayList.add(Model("4", "44", R.drawable.main_foot))
+
+        val myAdapter = MyAdapter(arrayList, this)
+
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.adapter = myAdapter
     }
 }
