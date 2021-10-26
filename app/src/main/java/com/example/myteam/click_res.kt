aa5bf6af9b5fdd5084f63ea93bfd8b5f085f4978
Ou ZhengHou <bunny8811033@gmail.com>
2021-10-26 15:58:33 +0800

    recyclerview item click

--- app/src/main/java/com/example/myteam/click_res.kt
+++ app/src/main/java/com/example/myteam/click_res.kt
@@ -3,6 +3,7 @@ package com.example.myteam
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_click_res.*
 
 class click_res : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +17,18 @@ class click_res : AppCompatActivity() {
         var intent = intent
         val aTitle = intent.getStringExtra("iTitle")
         val aDescription = intent.getStringExtra("iDescription")
-        val aImageView = intent.getStringExtra("iImageView")
+        val aImageView = intent.getIntExtra("iImageView",0)
+
+
 
         actionBar.setTitle(aTitle)
-//        a_title.text = aTitle
-//        a_description.text = aDescription
-//        imageView.setImageResource(aImageView)
+        a_title.text = aTitle
+        a_description.text = aDescription
+        imageView.setImageResource(aImageView)
 
         //返回到restaurant_list
-        supportActionBar!!.title="BehKhiSng"
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar!!.title="BehKhiSng"
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
     }
 }
