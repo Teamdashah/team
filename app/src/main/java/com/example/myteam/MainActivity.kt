e7eb3b1ccf3c7d684310c5d725bffda432636bef
acer <amyyyyy1234567@gmail.com>
2021-10-29 14:32:39 +0800

    嗨

--- app/src/main/java/com/example/myteam/MainActivity.kt
+++ app/src/main/java/com/example/myteam/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         //handle onClick
         lButton.setOnClickListener {
             //intent to start NewActivity
-            startActivity(Intent(this@MainActivity, login::class.java))
+            startActivity(Intent(this@MainActivity, hotel_main::class.java))
         }
 
         val rButton = findViewById<Button>(R.id.register_btn)
