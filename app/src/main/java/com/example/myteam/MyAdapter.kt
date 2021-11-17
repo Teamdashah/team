1fa9181f822d00411db38b5484c63ee502da6603
Hsu <a1073340@mail.nuk.edu.tw>
2021-11-17 16:29:31 +0800

    餐廳recyclerView，照片成功顯示

--- app/src/main/java/com/example/myteam/MyAdapter.kt
+++ app/src/main/java/com/example/myteam/MyAdapter.kt
@@ -2,17 +2,23 @@ package com.example.myteam
 
 import android.content.Context
 import android.content.Intent
+import android.icu.number.NumberFormatter.with
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.GenericTransitionOptions.with
+import com.bumptech.glide.Glide.with
 import com.example.myteam.model.restaurantData
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_login.view.*
 import kotlinx.android.synthetic.main.row.view.*
+import java.security.AccessController.getContext
 
 class MyAdapter(val restaurantList: ArrayList<restaurantData>) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
@@ -25,7 +31,10 @@ class MyAdapter(val restaurantList: ArrayList<restaurantData>) :
 
         val currentitem = restaurantList[position]
 
-        //holder.coverUrl.text = currentitem.coverUrl
+        val url = currentitem.coverUrl
+
+        Picasso.get().load(url).into(holder.imageView)
+
         holder.name.text = currentitem.name
         holder.avgPrice.text = currentitem.avgPrice.toString()
         holder.phone.text = currentitem.phone
@@ -43,7 +52,7 @@ class MyAdapter(val restaurantList: ArrayList<restaurantData>) :
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
-        //val coverUrl : TextView = itemView.findViewById(R.id.image_restaurant)
+        val imageView : ImageView = itemView.findViewById(R.id.resImage)
         val name : TextView = itemView.findViewById(R.id.title_restaurant)
         val avgPrice : TextView = itemView.findViewById(R.id.description_restaurant01)
         val phone : TextView = itemView.findViewById(R.id.description_restaurant02)
