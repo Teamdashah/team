fba76368e642b131f8291ecc9ef6656f724e85cb
Hsu <a1073340@mail.nuk.edu.tw>
2021-11-21 17:32:43 +0800

    google map路線顯示

--- app/src/main/java/com/example/myteam/GoogleMapsActivity.kt
+++ app/src/main/java/com/example/myteam/GoogleMapsActivity.kt
@@ -1,7 +1,13 @@
 package com.example.myteam
 
+import android.annotation.SuppressLint
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +16,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.myteam.databinding.ActivityGoogleMapsBinding
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import com.google.android.libraries.places.api.Places
+
 
 class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityGoogleMapsBinding
+
+    private var original = LatLng(22.713700639759832, 120.28196635763987)
+    private var destination = LatLng(22.725911, 120.282546)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +37,36 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityGoogleMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Fetching API_KEY which we wrapped
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["com.google.android.geo.API_KEY"]
+        val apiKey = value.toString()
+        // Initializing the Places API with the help of our API_KEY
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val gd = findViewById<Button>(R.id.directions)
+        gd.setOnClickListener{
+            mapFragment.getMapAsync {
+                mMap = it
+                //mMap.isMyLocationEnabled = true
+                val originLocation = original
+                mMap.addMarker(MarkerOptions().position(originLocation))
+                val destinationLocation = destination
+                mMap.addMarker(MarkerOptions().position(destinationLocation))
+                val urll = getDirectionURL(originLocation, destinationLocation, apiKey)
+                GetDirection(urll).execute()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 14F))
+            }
+        }
     }
 
     /**
@@ -37,13 +78,100 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
+    /*override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.isTrafficEnabled
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        val Taipei = LatLng(25.105497, 121.597366)
-        mMap.addMarker(MarkerOptions().position(Taipei).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Taipei, 10f))
+        val original = LatLng(22.725911, 120.282546)
+        val destination = LatLng(25.105497, 121.597366)
+        mMap.addMarker(MarkerOptions().position(destination).title("Marker in destination"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 10f))
+    }*/
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(original))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(original, 18F))
     }
+
+    private fun getDirectionURL(origin:LatLng, dest:LatLng, secret: String) : String{
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
+                "&destination=${dest.latitude},${dest.longitude}" +
+                "&sensor=false" +
+                "&mode=driving" +
+                "&key=$secret"
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body !!.string()
+
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,MapData::class.java)
+                val path =  ArrayList<LatLng>()
+                for (i in 0 until respObj.routes[0].legs[0].steps.size){
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.GREEN)
+                lineoption.geodesic(true)
+            }
+            mMap.addPolyline(lineoption)
+        }
+    }
+
+
+    fun decodePolyline(encoded: String): List<LatLng> {
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+        return poly
+    }
+
+
+
 }
