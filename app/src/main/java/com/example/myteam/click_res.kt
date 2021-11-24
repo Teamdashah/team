a761eb4d938a407efe7dd1434a42c034377de72c
acer <amyyyyy1234567@gmail.com>
2021-11-25 00:13:19 +0800

    餐廳營業時間加上去啦

--- app/src/main/java/com/example/myteam/click_res.kt
+++ app/src/main/java/com/example/myteam/click_res.kt
@@ -25,6 +25,13 @@ class click_res : AppCompatActivity() {
         val rDescription03 = intent.getStringExtra("address")
         val getresImgurl = intent.getStringExtra("coverUrl")
         val res_opentime = intent.getStringArrayListExtra("openingHoursList")
+        val Monday = res_opentime?.get(0)?.toString()
+        val Tuesday = res_opentime?.get(1)?.toString()
+        val Wednesday = res_opentime?.get(2)?.toString()
+        val Thurday = res_opentime?.get(3)?.toString()
+        val Friday = res_opentime?.get(4)?.toString()
+        val Saturday = res_opentime?.get(5)?.toString()
+        val Sunday = res_opentime?.get(6)?.toString()
 
 
 //        actionBar.setTitle(aTitle)
@@ -32,7 +39,7 @@ class click_res : AppCompatActivity() {
         restaurant_description01.text = rDescription01
         restaurant_description02.text = rDescription02
         restaurant_description03.text = rDescription03
-        restaurant_opentime.text = res_opentime.toString()
+        restaurant_opentime.text = Monday+"\n"+Tuesday+"\n"+Wednesday+"\n"+Thurday+"\n"+Friday+"\n"+Saturday+"\n"+Sunday
         Picasso.get().load(getresImgurl).into(restaurant_imageView)
 
 
