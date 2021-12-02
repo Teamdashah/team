faa23f5a9a5deeb5cccaf4c4b2c8864d857aa388
acer <amyyyyy1234567@gmail.com>
2021-12-03 02:32:05 +0800

    OKOK

---
+++ app/src/main/java/com/example/myteam/click_mylove_new.kt
@@ -0,0 +1,13 @@
+package com.example.myteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class click_mylove_new : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_click_mylove_new)
+
+    }
+}
